3b59b79faffa1322617e4ab370c366152e054c5f
Nihal <niftynihal@gmail.com>
2023-08-11 17:30:46 +0530

    code files

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require("express")
+const cors = require("cors")
+
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+
+app.get("/", (req, res) => {
+    res.send("Base API endpoint")
+})
+
+app.post("/submit", (req, res) => {
+    //TODO check if it's a valid JSON?
+    const payload = req.body;
+    res.send(payload)
+})
+
+app.listen(8000, () => {
+    console.log("listening on port 8000")
+})
